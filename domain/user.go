a553358a03595d494c49168f730ce7cb40c53363
Septiar <septiarmustafa@gmail.com>
2024-10-31 14:21:08 +0700

    domain layer user dan dto

--- domain/user.go
+++ domain/user.go
@@ -1,1 +1,24 @@
 package domain
+
+import (
+	"belajar-auth/dto"
+	"context"
+)
+
+type User struct {
+	ID       int64  `db:"id"`
+	FullName string `db:"full_name"`
+	Phone    string `db:"phone"`
+	Username string `db:"username"`
+	Password string `db:"password"`
+}
+
+type UserInterface interface {
+	FindByID(ctx context.Context, id int64) (User, error)
+	FindByUsername(ctx context.Context, username string) (User, error)
+}
+
+type UserService interface {
+	Authenticate(ctx context.Context, req dto.AuthReq) (dto.AuthRes, error)
+	ValidateToken(ctx context.Context, token string) (dto.UserData, error)
+}
