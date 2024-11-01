7c76445e032e2762135df0a09e6bda59dfc4213c
Septiar <septiarmustafa@gmail.com>
2024-11-01 10:25:36 +0700

    update model user, dto regist, user regist repository

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -35,3 +35,30 @@ func (u *UserRepository) FindByUsername(ctx context.Context, username string) (u
 	_, err = dataset.ScanStructContext(ctx, &user)
 	return
 }
+
+// Insert implements domain.UserRepository.
+func (u *UserRepository) Insert(ctx context.Context, user *domain.User) error {
+	executor := u.db.Insert("users").Rows(goqu.Record{
+		"full_name": user.FullName,
+		"email":     user.Email,
+		"phone":     user.Phone,
+		"username":  user.Username,
+		"password":  user.Password,
+	}).Returning("id").Executor()
+	_, err := executor.ScanStructContext(ctx, user)
+	return err
+}
+
+// Update implements domain.UserRepository.
+func (u *UserRepository) Update(ctx context.Context, user *domain.User) error {
+	executor := u.db.Update("users").Set(goqu.Record{
+		"full_name":         user.FullName,
+		"email":             user.Email,
+		"phone":             user.Phone,
+		"username":          user.Username,
+		"password":          user.Password,
+		"email_verified_at": user.EmailVerifiedAtDB,
+	}).Executor()
+	_, err := executor.ExecContext(ctx)
+	return err
+}
